bc9a8db3dfd3bf9e38870cda3b51fa8da0e30001
amit <amitvarshney_1@yahoo.co.in>
2025-11-05 05:01:06 +0000

    fixed ec2 tags and update output depends

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "amit-devops" {
   ami           = "ami-0a25a306450a2cba3"
   instance_type = "t2.nano"
   key_name      = "roche-key"
-  tags_all = {
+  tags = {
     "Name" = "amit-vm-1"
   }
 }
--- local_file.tf
+++ local_file.tf
@@ -2,5 +2,6 @@
 resource "local_file" "amit_local" {
   filename = "myip.txt"
   content  = aws_instance.amit-devops.public_ip
-
+  #making manual dependency
+  depends_on = [ aws_instance.amit-devops ]
 }
