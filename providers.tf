69b38fe55ed40996f8c51d8a917b4bdbc67ffb12
amit <amitvarshney_1@yahoo.co.in>
2025-11-05 06:09:03 +0000

    updated terraform.tfstate to s3+dynamodb

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  backend "s3" {
+    bucket = "amit-roche-tf-buck"
+    key = "dev/amit/terraform.tfstate"
+    dynamodb_table = "amit-roche-tb1"
+    region = "ap-southeast-2"
+    encrypt = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
