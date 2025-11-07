9eb1e41f9c19ce55a6f19ca05c17d736b58d3f12
amit <amitvarshney_1@yahoo.co.in>
2025-11-07 05:41:53 +0000

    editing ansible.txt file with a group

--- modules/ec2/local_file.tf
+++ modules/ec2/local_file.tf
@@ -1,7 +1,7 @@
 # storing data in local file
 resource "local_file" "amit_local" {
   filename = "${path.module}/ansible-inventory.txt"
-  content  = join("\n",aws_instance.amit-devops[*].public_ip)
+  content  = "[amit-roche]\n${join("\n",aws_instance.amit-devops[*].public_ip)}"
   #making manual dependency
   depends_on = [aws_instance.amit-devops]
 }
